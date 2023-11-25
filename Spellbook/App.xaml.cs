9c5c034f48016e2ae5a9b177a8051d80f317a8fc
Delian Zidarov <d.zidarov@gmail.com>
2023-11-25 22:49:51 +0900

    First

---
+++ Spellbook/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Spellbook;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ Spellbook/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Spellbook;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
